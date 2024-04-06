f5ff480497422f8bf778d4a3368e83ac49523d05
Piotr Szydełko <wiertel@wiertel.info>
2024-04-06 19:50:53 +0200

    further tuning, added combos for quote and tab

--- keyboards/gboards/gergoplex/keymaps/wiertel/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/wiertel/keymap.c
@@ -40,18 +40,17 @@ enum {
 #define HOME_L LALT_T(KC_L)
 #define HOME_SCLN RGUI_T(KC_SCLN)
 
-#define KC_CTRL_ESC MT(MOD_RCTL, KC_ESC)  // Tap for Esc, hold for GUI (Meta, Command, Win)
-#define KC_SPE_PRN LT(_SPECIAL, KC_LEFT_PAREN)   // Tap for Space, hold for Special layer
-#define KC_NUM_PRN LT(_NUMBERS, KC_RIGHT_PAREN)  // Tap for Space, hold for Numbers layer
+#define KC_SPE_ESC LT(_SPECIAL, KC_ESC)   // Tap for Esc, hold for Special layer
+#define KC_NUM_BKSP LT(_NUMBERS, KC_BACKSPACE)  // Tap for Backspace, hold for Numbers layer
 
     /* Combomap
      *
      * ,-------------------------------.      ,-------------------------------.
-     * |       |    ESC    |     |     |      |     |     |     |     |       |
+     * |       |    ESC    |     |     |      |     |     |    TAB    |       |
      * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
      * |       |     |     |     |     |      |     |     |     |     |       |
      * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
-     * |       |     |     |     |     |      |     |     |     |     |       |
+     * |       |     |     |     |     |      |     |     |     |     "       |
      * `-------------------------------'      `-------------------------------'
      *            .-----------------.            .-----------------.
      *            |     |     |     |            |     |     |     |
@@ -68,34 +67,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-------+-------+-------+-------+-----|      |-------+-------+-------+-------+---|
      * |   Z   |   X   |   C   |   V   |  B  |      |   N   |   M   |   ,   |   .   | / |
      * `-------------------------------------'      `-----------------------------------'
-     *              .-------------------------.    .---------------------.
-     *              | ESC CTR | ( NUM | ) SPE |    | ENT   | SPC   | ALT |
-     *              '-------------------------'    '---------------------'
+     *           .-------+----------+---------.    .-------+-------+-----.
+     *           | SHIFT | BKSP NUM | ESC SPE |    | ENT   | SPC   | ALT |
+     *           '-------+----------+---------'    '-------+-------+-----'
      */
     [_ALPHA] = LAYOUT_split_3x5_3(
          KC_Q,     KC_W,   KC_E,   KC_R,   KC_T,          KC_Y,   KC_U,   KC_I,     KC_O,   KC_P,
          HOME_A,   HOME_S, HOME_D, HOME_F, KC_G,          KC_H,   HOME_J, HOME_K,   HOME_L, HOME_SCLN,
          KC_Z,     KC_X,   KC_C,   KC_V,   KC_B,          KC_N,   KC_M,   KC_COMMA, KC_DOT, KC_SLASH,
-            KC_CTRL_ESC, KC_NUM_PRN, KC_SPE_PRN,          KC_ENTER, KC_SPACE, KC_RALT),
+            KC_LSFT, KC_NUM_BKSP, KC_SPE_ESC,             KC_ENTER, KC_SPACE, KC_RALT),
 
     /* Keymap 1: Special characters layer
      *
      * ,-------------------------------.      ,-------------------------------.
-     * |    !  |  @  |  {  |  }  |  |  |      |  `  |  ~  |     |     |   \   |
+     * |    !  |  @  |  {  |  }  |  |  |      |  `  |  ~  |  ?  |     |   \   |
      * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
      * |    #  |  $  |  (  |  )  | RMB |      |  +  |  -  |  /  |  *  |   '   |
      * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
      * |    %  |  ^  |  [  |  ]  | LMB |      |  &  |  =  |  ,  |  .  |   -   |
      * `-------------------------------'      `-------------------------------'
      *     .-------------------------.          .-----------------.
-     *     | ComboToggle |  ;  |  =  |          |  =  |  ;  | DEL |
+     *     | ComboToggle |  ;  |     |          |     |     | DEL |
      *     '-------------------------'          '-----------------'
      */
     [_SPECIAL] = LAYOUT_split_3x5_3(
-         KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,          KC_GRV,  KC_TILD, KC_TRNS, KC_TRNS, KC_BSLS,
+         KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE,          KC_GRV,  KC_TILD, KC_QUES, KC_TRNS, KC_BSLS,
          KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_BTN2,          KC_PLUS, KC_MINS, KC_SLSH, KC_ASTR, KC_QUOT,
          KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_BTN1,          KC_AMPR, KC_EQL,  KC_COMM, KC_DOT,  KC_MINS,
-                            CM_TOGG, KC_SCLN, KC_EQL,          KC_EQL, KC_SCLN, KC_DEL),
+                           CM_TOGG, KC_SCLN, KC_TRNS,          KC_TRNS, KC_TRNS, KC_DEL),
 
     /* Keymap 2: Numbers/Function/Motion layer
      *
@@ -107,12 +106,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |  F6   | F7  | F8  | F9  | F10 |      | MLFT| MDWN| MUP | MRGT| VOLDN |
      * `-------------------------------'      `-------------------------------'
      *             .-----------------.          .-----------------.
-     *             | F11 | F12 |     |          |     | PLY | SKP |
+     *             | F11 |     | F12 |          |     | PLY | CTR |
      *             '-----------------'          '-----------------'
      */
     [_NUMBERS] = LAYOUT_split_3x5_3(
          KC_1,  KC_2,  KC_3,  KC_4,  KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
          KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLU,
          KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_VOLD,
-                    KC_F11, KC_F12, KC_TRNS,         KC_TRNS,  KC_MPLY,  KC_MNXT)
+                    KC_F11, KC_TRNS, KC_F12,         KC_TRNS, KC_MPLY,  KC_RCTL)
 };
